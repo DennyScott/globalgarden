ba0467eeb15f362b646cdc998e1ecabd0bea8bc6
Gizmmo <travisscott301@gmail.com>
2014-03-30 16:14:55 -0500

    Added Plant AI

---
+++ client/helpers/querying.js
@@ -0,0 +1,28 @@
+function get_history_days(gardenId, numberOfDays, type){
+		var found = Gardens.findOne(gardenId);
+		type = type.toLowerCase();
+		var history;
+		if(found){
+			if(type === "temperature"){
+				history = getHistoryFromArray(numberOfDays, found.temp.dateHistory);
+			} else if(type === "humidity"){
+				history = getHistoryFromArray(numberOfDays, found.humidity.dateHistory);
+			} else if(type === "moisture"){
+				history = getHistoryFromArray(numberOfDays, found.moisture.dateHistory);
+			} else if(type === "light"){
+				history = getHistoryFromArray(numberOfDays, found.light.dateHistory);
+			}
+		}
+		return history;
+}
+
+function getHistoryFromArray(days, findArray){
+	var start = new Date(new Date().setDate(new Date().getDate()-days));
+	returnArray = [];
+	for(var i =0; i < findArray.length; i++){
+		if(findArray[i].date >= start){
+			returnArray[returnArray.length] = findArray[i];
+		}
+	}
+	return returnArray;
+}
