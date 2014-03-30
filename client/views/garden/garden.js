377b4c3dc9553fe1eea7821829864c8bcff2cf9a
Denny Scott <dennyscott301@gmail.com>
2014-03-30 16:27:17 -0500

    Added Sprinkler to toggle

--- client/views/garden/garden.js
+++ client/views/garden/garden.js
@@ -2,10 +2,16 @@ var globalTemperature = ['Global', 30, 20, 50, 40, 60, 50];
 var temperature,humidity,moisture,light, globalHumidity, globalMoisture, globalLight;
 var chart;
 
+Template.garden.created = function () {
+
+    Session.set("gardenID", 'vFab3DHPEPqhn4LYc');
+};
+
 Template.garden.rendered = function () {
 	blurlib();
 	loadd3();
 	loadc3();
+    
     Session.set("temperature", true);
     Session.set("humidity", false);
     Session.set("moisture", false);
@@ -13,10 +19,10 @@ Template.garden.rendered = function () {
     $('blur').each(function(){
       console.log("in blur");
       $(this).blurjs({
-       source: 'body',
-       radius: 20,
-       overlay: 'rgba(255,255,255,0.4)'
-   });
+         source: 'body',
+         radius: 20,
+         overlay: 'rgba(255,255,255,0.4)'
+     });
   });
 
     temperature = loadData("Your Garden - Temp", "temperature");
@@ -27,53 +33,54 @@ Template.garden.rendered = function () {
 
     chart = c3.generate({
         data: {
-            columns: [
-            globalTemperature,
-            temperature,
-            humidity,
-            moisture,
-            light
+          x: 'x',
+          columns: [
+          ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
+          globalTemperature,
+          temperature,
+          humidity,
+          moisture,
+          light
 
-            ]
-        },
-        zoom: {
-         enabled : true
-     },
-     axis : {
-        // x : {
-        //     type : 'timeseries'
-        // }
-    },
-    tooltip: {
-    	show: false
+          ]
+      },
+      zoom: {
+       enabled : true
+   },
+   axis : {
+    x : {
+        type : 'timeseries'
     }
+},
+tooltip: {
+   show: false
+}
 });
 
 
 
     Deps.autorun(function() {
 
-     temperature = loadData("Your Garden - Temp", "temperature", function(){return Gardens.findOne().temp;});
-     humididty = loadData("Your Garden - Humidity", "humididty", function(){return Gardens.findOne().humidity;});
-     moisture = loadData("Your Garden - Moisture", "moisture", function(){return Gardens.findOne().moisture;});
-     light = loadData("Your Garden - Light", "light", function(){return Gardens.findOne().light;});
-     redraw();
+       temperature = loadData("Your Garden - Temp", "temperature");
+       humidity = loadData("Your Garden - Humidity", "humididty");
 
- });
+       moisture = loadData("Your Garden - Moisture", "moisture");
+       light = loadData("Your Garden - Light", "light");
+       redraw();
+
+   });
 };
 
 function loadData(name, attribute){
     var data;
     if(!Session.get(attribute)){
-     data = [];
- }else{
+       data = [];
+   }else{
     console.log(attribute);
-    data = get_history_days('vFab3DHPEPqhn4LYc',30, attribute);
+    data = get_history_days(Session.get('gardenID'),30, attribute);
     console.log(data);
 }
-
-    var nameObject = {date: new Date(), val: name};
-    data.unshift(nameObject);
+data.unshift(name);
 return data;
 }
 
@@ -81,6 +88,7 @@ function redraw(){
 	
 	chart.load({
         columns: [
+        ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
         globalTemperature,
         temperature,
         humidity,
@@ -89,3 +97,27 @@ function redraw(){
         ]
     });
 }
+
+Template.garden.helpers({
+    isSprinklerOn: function () {
+        var gardenObject = Gardens.findOne({_id: Session.get('gardenID')});
+        console.log(gardenObject);
+        if(gardenObject.sprinklerOn){
+            return "good";
+        }else{
+            return "danger";
+        }
+    }
+});
+
+Template.garden.events({
+    'click #toggle-sprinkler': function () {
+        var gardenObject = Gardens.findOne({_id: Session.get('gardenID')});
+        if(!gardenObject.sprinklerOn){
+
+            Meteor.call("toggleSprinkler", Session.get('gardenID'), "medium", "warm");
+        }else{
+            
+        }
+    }
+});
