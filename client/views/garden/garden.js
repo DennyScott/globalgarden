0ec7efd034d9c4249e2450b922626759ab457728
Denny Scott <dennyscott301@gmail.com>
2014-03-30 07:57:34 -0500

    Added General Interface

--- client/views/garden/garden.js
+++ client/views/garden/garden.js
@@ -1,5 +1,10 @@
+var global = ['Global', 30, 20, 50, 40, 60, 50];
+var chart;
+
 Template.garden.rendered = function () {
 	blurlib();
+	loadd3();
+	loadc3();
 
 	$('blur').each(function(){
 		console.log("in blur");
@@ -9,4 +14,55 @@ Template.garden.rendered = function () {
 			overlay: 'rgba(255,255,255,0.4)'
 		});
 	});
+
+	var user = loadData("Your Garden");
+
+	chart = c3.generate({
+    data: {
+        x: 'x',
+        columns: [
+            ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
+            global,
+            user
+
+        ]
+    },
+    zoom: {
+    	enabled : true
+    },
+    axis : {
+        x : {
+            type : 'timeseries'
+        }
+    },
+    tooltip: {
+    	show: false
+    }
+});
+
+	
+
+	Deps.autorun(function() {
+		var user = loadData("Your Garden");
+		redraw(user);
+		
+	});
 };
+
+function loadData(name){
+	var data = Gardens.findOne({}).temp;
+
+	data.unshift(name);
+	return data;
+}
+
+function redraw(user){
+	
+	chart.load({
+        columns: [
+         ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
+            global,
+            user
+        ]
+    });
+}
